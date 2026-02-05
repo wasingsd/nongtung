c75c1e6c1a541e8c96dd2bc884bf4098feef5798
wasingsd <wasin.gsd@gmail.com>
2026-02-05 10:03:45 +0700

    security: move firebase config to env variables and update with new regenerated key

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,13 +6,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCScIIzkPsBblB3a9gXif2-LNfcTxQJI80",
-    authDomain: "nongtung-platform.firebaseapp.com",
-    projectId: "nongtung-platform",
-    storageBucket: "nongtung-platform.firebasestorage.app",
-    messagingSenderId: "568276841852",
-    appId: "1:568276841852:web:462543f3ef2d26cab3a0f9",
-    measurementId: "G-B7MJ6ZFQ6S"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
